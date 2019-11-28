79e98320f763be3d6d6182bc323f7cdac5a91d55
John Richardson <jvrdev@outlook.com>
2019-11-28 01:13:58 -0800

    added basic routes

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Welcome to Index'
+
+@app.route('/about')
+def about():
+    return 'About Page'
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page'
+
+@app.route('/blog')
+def blog():
+    return "Blog Page"
+
+@app.route('/contact')
+def contact():
+    return "Contact page"
